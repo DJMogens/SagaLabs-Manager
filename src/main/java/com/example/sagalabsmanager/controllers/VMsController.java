e3c14d48d07461055406227ac569db9eb8123b8e
lyngdorf <sofie_lyngdorf@hotmail.com>
2023-03-04 15:55:33 +0100

    Fully scalable tabs on labs page, scales by number of labs found in Azure

--- src/main/java/com/example/sagalabsmanager/controllers/VMsController.java
+++ src/main/java/com/example/sagalabsmanager/controllers/VMsController.java
@@ -1,26 +1,50 @@
 package com.example.sagalabsmanager.controllers;
 
+import com.azure.resourcemanager.network.models.VM;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
 import com.example.sagalabsmanager.AzureLogin;
 import com.example.sagalabsmanager.AzureMethods;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.util.ArrayList;
 
 public class VMsController extends MenuController {
-
-    @FXML public Tab Lab1;
-    @FXML public Tab Lab2;
+    @FXML public TabPane tabPane;
+    ArrayList<ResourceGroup> allLabs;
+    ArrayList<Tab> tabs = new ArrayList<Tab>();
 
     public void initialize() {
-        initializeTabNames();
+        initializeTabs();
     }
-    private void initializeTabNames() {
-        ArrayList<ResourceGroup> allLabs = AzureMethods.getAllLabs(AzureLogin.azure);
-        Tab[] tabs = {Lab1, Lab2};
-        for(int i = 0; i < tabs.length; i++) {
-            tabs[i].setText(allLabs.get(i).name().substring(0, 10)); // Sets text of tabs to first 10 characters of lab names
+
+    private void initializeTabs() {
+        allLabs = AzureMethods.getAllLabs(AzureLogin.azure);
+        for(ResourceGroup lab: allLabs) {
+            Tab tab = new Tab();
+            tab.setText(lab.name().substring(0, 10));
+
+            TableView<VM> tableView = new TableView<VM>();
+            tab.setContent(tableView);
+
+            initializeColumns(tableView);
+
+            tabPane.getTabs().add(tab);
+            tabs.add(tab);
         }
+    }
+    private void initializeColumns(TableView tableView) {
+        TableColumn idColumn = new TableColumn("ID");
+        idColumn.setPrefWidth(450.0);
+        TableColumn vmColumn = new TableColumn("Machine Name");
+        vmColumn.setPrefWidth(450.0);
+        TableColumn osColumn = new TableColumn("OS");
+        osColumn.setPrefWidth(199.0);
+        tableView.getColumns().add(idColumn);
+        tableView.getColumns().add(vmColumn);
+        tableView.getColumns().add(osColumn);
     }
 }
