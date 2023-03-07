fa8d453f8d3643b817b9f8e4297ad0f61ef9107b
DJMogens <dani788c@gmail.com>
2023-03-07 20:31:42 +0100

    Delete server side things from client side

--- SagaLabs-Manager-Client/src/main/java/sagalabsmanagerclient/Database.java
+++ SagaLabs-Manager-Client/src/main/java/sagalabsmanagerclient/Database.java
@@ -29,72 +29,9 @@ public class Database {
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
-
             }
             return false;
         }
     }
 
-    public static void syncLabs() throws SQLException {
-        Connection conn = DriverManager.getConnection(DB_URL, dbUsername, dbPassword);
-        List<ResourceGroup> labs = AzureLogin.azure.resourceGroups().listByTag("lab", "true").stream().toList();
-
-        String truncateSql = "TRUNCATE TABLE Labs";
-        PreparedStatement truncateStmt = conn.prepareStatement(truncateSql);
-        truncateStmt.executeUpdate();
-        int id = 0;
-        for (ResourceGroup lab : labs) {
-            String labName = lab.name();
-            String labID = lab.id();
-            int vmCount = AzureLogin.azure.virtualMachines().listByResourceGroup(labName).stream().toList().size();
-            List<PublicIpAddress> publicIps = AzureLogin.azure.publicIpAddresses().listByResourceGroup(labName).stream().toList();
-            //iterate over all public ip's in each lab, and find the ip that is contains VPN in the name
-            String vpnPublicIp = "No public IP";
-            for (PublicIpAddress publicIp : publicIps) {
-                String publicIpName = publicIp.name();
-                if (publicIpName.contains("VPN")) {
-                    vpnPublicIp = publicIp.ipAddress();
-                    break;
-                }
-            }
-
-            String sql = "INSERT INTO Labs (LabName,LabID,id,VmCount,LabVPN) VALUES (?,?,?,?,?)";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, labName);
-            stmt.setString(2, labID);
-            stmt.setInt(3, id);
-            stmt.setInt(4, vmCount);
-            stmt.setString(5, vpnPublicIp);
-            stmt.executeUpdate();
-            id++;
-        }
-        syncVM();
-
-    }
-
-    public static void syncVM() throws SQLException {
-        Connection conn = DriverManager.getConnection(DB_URL, dbUsername, dbPassword);
-
-        List<ResourceGroup> labs = AzureLogin.azure.resourceGroups().listByTag("lab", "true").stream().toList();
-
-        //refresh database
-        String truncateSql = "TRUNCATE TABLE vm";
-        PreparedStatement truncateStmt = conn.prepareStatement(truncateSql);
-        truncateStmt.executeUpdate();
-
-        //for each resourcegroup that it a lab; for each virtual machine
-        for (ResourceGroup rg : AzureLogin.azure.resourceGroups().listByTag("lab", "true")) {
-
-            // Get all virtual machines in the resource group
-            for (VirtualMachine vm : AzureLogin.azure.virtualMachines().listByResourceGroup(rg.name())) {
-                String sql = "INSERT INTO vm (vm_name, powerstate, internal_ip, ostype) VALUES (?,?,?,?)";
-                PreparedStatement stmt = conn.prepareStatement(sql);
-                stmt.setString(1, vm.name());
-                stmt.setString(2, vm.powerState().toString());
-                stmt.setString(3, vm.osType().toString());
-                stmt.setString(4, vm.getPrimaryNetworkInterface().primaryPrivateIP());
-                stmt.executeUpdate();
-            }
-        }
-    }
 }
