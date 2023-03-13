b84b717efb3638750b726d15f6edec77db986644
lyngdorf <sofie_lyngdorf@hotmail.com>
2023-03-13 14:47:13 +0100

    Machines tab has filters OS and state working

--- SagaLabs-Manager-Client/src/main/java/sagalabsmanagerclient/controllers/MachinesController.java
+++ SagaLabs-Manager-Client/src/main/java/sagalabsmanagerclient/controllers/MachinesController.java
@@ -3,6 +3,9 @@ package sagalabsmanagerclient.controllers;
 import com.azure.resourcemanager.compute.models.VirtualMachine;
 import com.azure.resourcemanager.containerservice.models.OSType;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import org.w3c.dom.Text;
 import sagalabsmanagerclient.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -11,11 +14,18 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class MachinesController extends MenuController {
     @FXML protected TabPane tabPane;
     @FXML protected Tab allTab;
     @FXML protected TableView<MachinesVM> allTableView;
+    @FXML protected TextField osFilterText;
+    @FXML protected Button filterButton;
+    @FXML protected TextField stateFilterText;
 
     public static ArrayList<MachinesTab> machinesTabs = new ArrayList<MachinesTab>();
 
@@ -97,16 +107,22 @@ public class MachinesController extends MenuController {
         }
     }
 
-    // METHOD TO RETURN VMs WITH CHECKMARK. Currently returns MachinesVM objects.
-    private ArrayList<MachinesVM> getSelectedVMs() {
-        MachinesTab machinesTab = machinesTabs.get(0); // By default all tab
-        ArrayList<MachinesVM> machineList = new ArrayList<MachinesVM>();
+    private MachinesTab getCurrentTab() {
+        MachinesTab machinesTab = new MachinesTab(null, null, null);
         for(MachinesTab tab: machinesTabs) {
             if(tab.getTab().isSelected()) {
                 machinesTab = tab;
                 break;
             }
         }
+        return machinesTab;
+    }
+
+    // METHOD TO RETURN VMs WITH CHECKMARK. Currently returns MachinesVM objects.
+    private ArrayList<MachinesVM> getSelectedVMs() {
+        MachinesTab machinesTab = machinesTabs.get(0); // By default all tab
+        ArrayList<MachinesVM> machineList = new ArrayList<MachinesVM>();
+        machinesTab = getCurrentTab();
         for (MachinesVM vm :machinesTab.getTableView().getItems()) {
             if(vm.getSelected()) {
                 machineList.add(vm);
@@ -115,4 +131,21 @@ public class MachinesController extends MenuController {
         return machineList;
     }
 
+    public void applyFilter(ActionEvent actionEvent) {
+        System.out.println("applying filter");
+        MachinesTab tab = getCurrentTab();
+
+        String osFilter = osFilterText.getText();
+        String finalOsFilter = osFilter.replaceAll("\\s", "");
+        Predicate<MachinesVM> test = e -> e.getOs().equals(finalOsFilter);
+        FilteredList<MachinesVM> VMs = new FilteredList<MachinesVM>(tab.getTableView().getItems(), test);
+
+        String stateFilter = stateFilterText.getText();
+        String finalStateFilter = stateFilter.replaceAll("\\s", "");
+        Predicate<MachinesVM> test2 = e -> e.getState().equals(finalStateFilter);
+        VMs = new FilteredList<MachinesVM>(VMs, test2);
+
+        System.out.println("filtered list made");
+        tab.getTableView().setItems(VMs);
+    }
 }
