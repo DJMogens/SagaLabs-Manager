f98076fc6389c3ad4cca64774b0645dd5ea454df
DJMogens <dani788c@gmail.com>
2023-03-21 12:22:32 +0100

    Refactor complicated code in HomeController and added comments

--- SagaLabs-Manager-Client/src/main/java/sagalabsmanagerclient/controllers/HomeController.java
+++ SagaLabs-Manager-Client/src/main/java/sagalabsmanagerclient/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package sagalabsmanagerclient.controllers;
 
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -22,83 +21,107 @@ public class HomeController extends MenuController {
     private AnchorPane anchorHome;
 
     public void initialize() throws SQLException {
+        VBox vbox = createLabsVBox();
+        anchorHome.getChildren().add(vbox);
+    }
+
+    private VBox createLabsVBox() throws SQLException {
         // Execute a SQL query to retrieve information about labs
         ResultSet labs = Database.executeSql("SELECT LabName, vpnRunning, VmCount FROM Labs");
-
         // Create a VBox to hold all the lab information boxes
         VBox vbox = new VBox();
         vbox.setSpacing(10);
-
-        // Loop through the labs ResultSet and create an HBox for each lab
+        // Loop through the labs ResultSet
         while (labs.next()) {
-            // Retrieve lab information from the ResultSet
-            String labName = labs.getString("LabName");
-            boolean vpnRunning = labs.getBoolean("vpnRunning");
-            int vmCount = labs.getInt("VmCount");
-
-            // Create an HBox to hold all the lab information elements
-            HBox hbox = new HBox();
-            hbox.setSpacing(10);
-            hbox.setStyle("-fx-background-color: gray; -fx-border-color: #333333; -fx-border-width: 2px; -fx-border-style: solid;");
-
-            // Create a colored Rectangle to represent the VPN status
-            Rectangle box = new Rectangle(100, 100, vpnRunning ? Color.GREEN : Color.BLUE);
-
-            // Create a VBox to hold all the lab labels
-            VBox labelBox = new VBox();
-            labelBox.setSpacing(5);
-            Label nameLabel = new Label(labName);
-            nameLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
-            Label vmLabel = new Label("VM's: " + vmCount);
-            vmLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-            Label vpnStatusLabel = new Label(vpnRunning ? "VPN: Running" : "VPN: Down");
-            vpnStatusLabel.setTextFill(vpnRunning ? Color.GREEN : Color.RED);
-
-            labelBox.getChildren().addAll(nameLabel, vmLabel, vpnStatusLabel);
-
-            // Create buttons to turn on/off all VMs for this lab
-            Button turnOnButton = new Button("Turn On all machines");
-            Button turnOffButton = new Button("Turn Off all machines");
-
-            // Create a label to display startup status
-            Label statusLabel = new Label("Starting up...");
-            statusLabel.setVisible(false);
-
-            // Set up the turn on button event handler
-            turnOnButton.setOnAction(event -> {
-                Runnable task = () -> {
-                    AzureMethods.turnOnInLab(labName);
-                    Platform.runLater(() -> {
-                        // Update UI with success message
-                    });
-                };
-
-                Thread thread = new Thread(task);
-                thread.start();
-            });
-
-            // Set up the turn off button event handler
-            turnOffButton.setOnAction(event -> {
-                Runnable task = () -> {
-                    AzureMethods.turnOffVMsInLab(labName);
-                    Platform.runLater(() -> {
-                        // Update UI with success message
-                    });
-                };
-
-                Thread thread = new Thread(task);
-                thread.start();
-            });
-
-            // Add all the elements to the HBox
-            hbox.getChildren().addAll(box, labelBox, turnOnButton, turnOffButton);
-
-            // Add the HBox to the VBox
+            // Create an HBox for each lab containing lab information and controls
+            HBox hbox = createLabHBox(labs);
+            // Add the created HBox to the VBox
             vbox.getChildren().add(hbox);
         }
+        // Return the created VBox containing lab information boxes
+        return vbox;
+    }
 
-        // Add the VBox to the anchor pane in the scene
-        anchorHome.getChildren().add(vbox);
+
+    private HBox createLabHBox(ResultSet labs) throws SQLException {
+        // Retrieve lab information from the ResultSet
+        String labName = labs.getString("LabName");
+        boolean vpnRunning = labs.getBoolean("vpnRunning");
+        int vmCount = labs.getInt("VmCount");
+        // Create an HBox to hold all the lab information elements
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: gray; -fx-border-color: #333333; -fx-border-width: 2px; -fx-border-style: solid;");
+        // Create a colored Rectangle to represent the VPN status
+        Rectangle box = createVpnStatusRectangle(vpnRunning);
+        // Create a VBox to hold all the lab labels
+        VBox labelBox = createLabelsVBox(labName, vpnRunning, vmCount);
+        // Create buttons to turn on/off all VMs for this lab
+        Button turnOnButton = createTurnOnButton(labName);
+        Button turnOffButton = createTurnOffButton(labName);
+        // Add all the elements to the HBox
+        hbox.getChildren().addAll(box, labelBox, turnOnButton, turnOffButton);
+        // Return the created HBox containing lab information and controls
+        return hbox;
+    }
+
+
+    private Rectangle createVpnStatusRectangle(boolean vpnRunning) {
+        return new Rectangle(100, 100, vpnRunning ? Color.GREEN : Color.BLUE);
+    }
+
+    private VBox createLabelsVBox(String labName, boolean vpnRunning, int vmCount) {
+        // Create a VBox to hold all the lab labels
+        VBox labelBox = new VBox();
+        labelBox.setSpacing(5);
+        // Create a label for the lab name and set its style
+        Label nameLabel = new Label(labName);
+        nameLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        // Create a label for the number of VMs and set its style
+        Label vmLabel = new Label("VM's: " + vmCount);
+        vmLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+        // Create a label for the VPN status and set its color
+        Label vpnStatusLabel = new Label(vpnRunning ? "VPN: Running" : "VPN: Down");
+        vpnStatusLabel.setTextFill(vpnRunning ? Color.GREEN : Color.RED);
+        // Add all the labels to the VBox
+        labelBox.getChildren().addAll(nameLabel, vmLabel, vpnStatusLabel);
+        // Return the created VBox containing lab labels
+        return labelBox;
+    }
+
+
+    private Button createTurnOnButton(String labName) {
+        // Create a button to turn on all machines in the lab
+        Button turnOnButton = new Button("Turn On all machines");
+        // Set up the event handler for the button
+        turnOnButton.setOnAction(event -> {
+            // Create a Runnable task to turn on all VMs in the specified lab
+            Runnable task = () -> {
+                AzureMethods.turnOnInLab(labName);
+            };
+            // Start a new thread to execute the task
+            Thread thread = new Thread(task);
+            thread.start();
+        });
+        // Return the created button with the event handler
+        return turnOnButton;
+    }
+
+    private Button createTurnOffButton(String labName) {
+        // Create a button to turn off all machines in the lab
+        Button turnOffButton = new Button("Turn Off all machines");
+        // Set up the event handler for the button
+        turnOffButton.setOnAction(event -> {
+            // Create a Runnable task to turn off all VMs in the specified lab
+            Runnable task = () -> {
+                AzureMethods.turnOffVMsInLab(labName);
+            };
+            // Start a new thread to execute the task
+            Thread thread = new Thread(task);
+            thread.start();
+        });
+        // Return the created button with the event handler
+        return turnOffButton;
     }
 
 }
