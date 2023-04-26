0ebf50fe0f27acd7d62825a9647418832961fbef
DJMogens <dani788c@gmail.com>
2023-04-26 13:26:24 +0200

    Rollback: timeout isnt a good idea

--- SagaLabs-Manager-Server/src/main/java/sagalabsmanagerserver/Main.java
+++ SagaLabs-Manager-Server/src/main/java/sagalabsmanagerserver/Main.java
@@ -1,7 +1,9 @@
 package sagalabsmanagerserver;
 
 import java.sql.SQLException;
-import java.util.concurrent.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static sagalabsmanagerserver.Database.syncLabs;
 
@@ -16,44 +18,24 @@ public class Main {
 
     public static void startSyncLabs() {
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        Runnable syncLabsTask = () -> {
+        executor.scheduleAtFixedRate(() -> {
             try {
                 Database.syncLabs();
                 System.out.println("Synced Labs table");
             } catch (Exception e) {
                 System.out.println("Error in syncLabs: " + e.getMessage());
             }
-        };
-
-        executor.scheduleWithFixedDelay(() -> {
-            Future<?> future = executor.submit(syncLabsTask);
-            try {
-                future.get(60, TimeUnit.SECONDS);
-            } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                future.cancel(true);
-                System.out.println("SyncLabs task timed out. Restarting...");
-            }
         }, 0, 5, TimeUnit.SECONDS);
     }
 
     public static void startSyncVM() {
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        Runnable syncVMTask = () -> {
+        executor.scheduleAtFixedRate(() -> {
             try {
                 Database.syncVM();
                 System.out.println("Synced vm table");
             } catch (Exception e) {
                 System.out.println("Error in syncVM: " + e.getMessage());
-            }
-        };
-
-        executor.scheduleWithFixedDelay(() -> {
-            Future<?> future = executor.submit(syncVMTask);
-            try {
-                future.get(60, TimeUnit.SECONDS);
-            } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                future.cancel(true);
-                System.out.println("SyncVM task timed out. Restarting...");
             }
         }, 0, 5, TimeUnit.SECONDS);
     }
