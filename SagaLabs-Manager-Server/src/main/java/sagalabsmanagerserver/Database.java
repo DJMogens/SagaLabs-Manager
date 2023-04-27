5d6d893f416ee901f03abbb105a9ff38f9cce595
DJMogens <dani788c@gmail.com>
2023-04-27 12:11:46 +0200

    Introduce connection pooling

--- SagaLabs-Manager-Server/src/main/java/sagalabsmanagerserver/Database.java
+++ SagaLabs-Manager-Server/src/main/java/sagalabsmanagerserver/Database.java
@@ -4,35 +4,44 @@ import com.azure.resourcemanager.compute.models.PowerState;
 import com.azure.resourcemanager.compute.models.VirtualMachine;
 import com.azure.resourcemanager.network.models.PublicIpAddress;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.net.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Database {
 
     // Database credentials
+    // Database credentials
     private static final String DB_URL = "jdbc:mysql://sagadb.sagalabs.dk:42069/sagadb";
     private static final String dbUsername = "sagalabs-manager";
     static String dbPassword = AzureMethods.getKeyVaultSecret("sagalabs-manager-SQL-pw");
-    static Connection conn;
+    static HikariDataSource dataSource; // used for connection pooling
 
     static {
-        try {
-            conn = DriverManager.getConnection(DB_URL, dbUsername, dbPassword);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(DB_URL);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+        config.setMaximumPoolSize(10);
+
+        dataSource = new HikariDataSource(config);
     }
+
 
     public static void syncLabs() throws SQLException {
         System.out.println("Starting syncLabs...");
         long startTime = System.currentTimeMillis();
 
         String tableName = "Labs";
-
         List<ResourceGroup> labs = AzureLogin.azure.resourceGroups().listByTag("lab", "true").stream().toList();
 
         // Get all labs in the database
@@ -43,7 +52,8 @@ public class Database {
         // Prepare batch update
         String sql = "INSERT INTO Labs (LabName, LabID, VmCount, LabVPN, vpnRunning, azureID) VALUES (?, ?, ?, ?, ?, ?) " +
                 "ON DUPLICATE KEY UPDATE LabName=VALUES(LabName), LabID=VALUES(LabID), VmCount=VALUES(VmCount), LabVPN=VALUES(LabVPN), vpnRunning=VALUES(vpnRunning), azureID=VALUES(azureID)";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
             labs.parallelStream().forEach(lab -> {
                 if (lab == null) {
                     return;
@@ -52,14 +62,16 @@ public class Database {
                 String labID = lab.id() != null ? lab.id() : "";
 
                 int vmCount = 0;
-                if (labName != null && !labName.isEmpty()) {
+                if (!labName.isEmpty()) {
                     vmCount = AzureLogin.azure.virtualMachines().listByResourceGroup(labName).stream().toList().size();
                 }
+
                 List<PublicIpAddress> publicIps = new ArrayList<>();
-                if (labName != null && !labName.isEmpty()) {
+                if (!labName.isEmpty()) {
                     publicIps = AzureLogin.azure.publicIpAddresses().listByResourceGroup(labName).stream().toList();
                 }
-                //iterate over all public ip's in each lab, and find the ip that is contains VPN in the name
+
+                // Iterate over all public IPs in each lab and find the IP that contains VPN in the name
                 String vpnPublicIp = "No public IP";
                 boolean vpnRunning = false;
                 for (PublicIpAddress publicIp : publicIps) {
@@ -72,9 +84,7 @@ public class Database {
                         try {
                             Socket socket = new Socket();
                             socket.connect(new InetSocketAddress(vpnPublicIp, port), 2000);
-                            if (socket.isConnected()) {
-                                vpnRunning = true;
-                            }
+                            vpnRunning = socket.isConnected();
                             socket.close();
                         } catch (Exception ignored) {
                         }
@@ -91,7 +101,7 @@ public class Database {
                     stmt.setString(6, labID);
                     stmt.addBatch();
 
-                    if (labID != null && !labID.isEmpty()) {
+                    if (!labID.isEmpty()) {
                         dbLabs.remove(labID);
                     }
                     System.out.println("Lab synced: " + labName);
@@ -121,68 +131,84 @@ public class Database {
         // Get all labs in Azure
         List<ResourceGroup> labs = AzureLogin.azure.resourceGroups().listByTag("lab", "true").stream().toList();
 
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
         // Process virtual machines in parallel
-        labs.parallelStream().forEach(rg -> {
-            // Get all virtual machines in the resource group
-            List<VirtualMachine> virtualMachines = AzureLogin.azure.virtualMachines().listByResourceGroup(rg.name()).stream().toList();
+        List<Future<?>> futures = new ArrayList<>();
+        for (ResourceGroup rg : labs) {
+            futures.add(executor.submit(() -> {
+                // Get all virtual machines in the resource group
+                List<VirtualMachine> virtualMachines = AzureLogin.azure.virtualMachines().listByResourceGroup(rg.name()).stream().toList();
 
-            System.out.println("Processing resource group: " + rg.name());
+                System.out.println("Processing resource group: " + rg.name());
 
-            // Prepare batch update
-            String sql = "INSERT INTO vm (vm_name, powerstate, internal_ip, ostype, resource_group, azureID) VALUES (?,?,?,?,?,?) " +
-                    "ON DUPLICATE KEY UPDATE powerstate=VALUES(powerstate), internal_ip=VALUES(internal_ip), ostype=VALUES(ostype), resource_group=VALUES(resource_group),azureID=VALUES(azureID)";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                for (VirtualMachine vm : virtualMachines) {
-                    try {
-                        if (vm == null) {
-                            continue;
+                // Prepare batch update
+                String sql = "INSERT INTO vm (vm_name, powerstate, internal_ip, ostype, resource_group, azureID) VALUES (?,?,?,?,?,?) " +
+                        "ON DUPLICATE KEY UPDATE powerstate=VALUES(powerstate), internal_ip=VALUES(internal_ip), ostype=VALUES(ostype), resource_group=VALUES(resource_group),azureID=VALUES(azureID)";
+                try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    for (VirtualMachine vm : virtualMachines) {
+                        try {
+                            if (vm == null) {
+                                continue;
+                            }
+                            stmt.setString(1, vm.name());
+                            PowerState powerState = vm.powerState();
+                            if (powerState != null) {
+                                stmt.setString(2, powerState.toString());
+                            } else {
+                                stmt.setString(2, "");
+                            }
+                            if (vm.getPrimaryNetworkInterface() != null) {
+                                String internalIp = vm.getPrimaryNetworkInterface().primaryPrivateIP();
+                                stmt.setString(3, internalIp != null ? internalIp : "");
+                            } else {
+                                stmt.setString(3, "");
+                            }
+                            stmt.setString(4, vm.osType() != null ? vm.osType().toString() : "");
+                            stmt.setString(5, vm.resourceGroupName() != null ? vm.resourceGroupName() : "");
+                            stmt.setString(6, vm.id() != null ? vm.id() : "");
+                            stmt.addBatch();
+                            if (vm.id() != null && !vm.id().isEmpty()) {
+                                dbVMs.remove(vm.id());
+                            }
+                            System.out.println("VM synced: " + vm.name());
+                        } catch (Exception e) {
+                            System.out.println("Error syncing VM: " + vm.name() + " | " + e.getMessage());
                         }
-                        stmt.setString(1, vm.name());
-                        PowerState powerState = vm.powerState();
-                        if (powerState != null) {
-                            stmt.setString(2, powerState.toString());
-                        } else {
-                            stmt.setString(2, "");
-                        }
-                        if (vm.getPrimaryNetworkInterface() != null) {
-                            String internalIp = vm.getPrimaryNetworkInterface().primaryPrivateIP();
-                            stmt.setString(3, internalIp != null ? internalIp : "");
-                        } else {
-                            stmt.setString(3, "");
-                        }
-                        stmt.setString(4, vm.osType() != null ? vm.osType().toString() : "");
-                        stmt.setString(5, vm.resourceGroupName() != null ? vm.resourceGroupName() : "");
-                        stmt.setString(6, vm.id() != null ? vm.id() : "");
-                        stmt.addBatch();
-                        if (vm.id() != null && !vm.id().isEmpty()) {
-                            dbVMs.remove(vm.id());
-                        }
-                        System.out.println("VM synced: " + vm.name());
-                    } catch (Exception e) {
-                        System.out.println("Error syncing VM: " + vm.name() + " | " + e.getMessage());
                     }
+                    stmt.executeBatch();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
                 }
-                stmt.executeBatch();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+            }));
+        }
+
+        futures.forEach(future -> {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
         });
+
+        executor.shutdown();
 
         // Delete VMs from the database that are no longer in Azure
         for (String vmId : dbVMs) {
             String sql = "DELETE FROM " + tableName + " WHERE azureID = ?";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, vmId);
-            System.out.println("Deleting VM from database: " + vmId);
+            try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, vmId);
+                System.out.println("Deleting VM from database: " + vmId);
 
-            stmt.executeUpdate();
+                stmt.executeUpdate();
+            }
         }
 
         updateLastUpdate(tableName);
 
         long elapsedTime = System.currentTimeMillis() - startTime;
         if (elapsedTime > 30_000) {
-            System.out.printf("syncLabs took longer than 30 seconds to execute: %d ms%n", elapsedTime);
+            System.out.printf("syncVM took longer than 30 seconds to execute: %d ms%n", elapsedTime);
         }
 
         System.out.println("syncVM complete.");
@@ -191,25 +217,27 @@ public class Database {
 
 
 
+
     public static void updateLastUpdate(String tableName) throws SQLException {
         String sql = "INSERT INTO last_updated (table_name, last_update) VALUES (?, NOW()) " +
                 "ON DUPLICATE KEY UPDATE last_update=NOW()";
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        stmt.setString(1, tableName);
-        stmt.executeUpdate();
+        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, tableName);
+            stmt.executeUpdate();
+        }
     }
 
     public static List<String> getAllResourceIds(String tableName) throws SQLException {
         List<String> resourceIds = new ArrayList<>();
         String sql = "SELECT azureID FROM " + tableName;
-        Statement stmt = conn.createStatement();
-        ResultSet resultSet = stmt.executeQuery(sql);
+        try (Connection conn = dataSource.getConnection(); Statement stmt = conn.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery(sql);
 
-        while (resultSet.next()) {
-            resourceIds.add(resultSet.getString("azureID"));
+            while (resultSet.next()) {
+                resourceIds.add(resultSet.getString("azureID"));
+            }
         }
         return resourceIds;
     }
-
 
 }
